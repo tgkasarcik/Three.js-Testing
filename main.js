a07a9502f216c236cfc37aa269439720a43d412c
Tommy Kasarcik <77713266+tgkasarcik@users.noreply.github.com>
2024-07-27 15:37:30 -0400

    More experimenting.

--- main.js
+++ main.js
@@ -1,28 +1,58 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 30;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+const renderer = new THREE.WebGLRenderer({
+	canvas: document.querySelector('#canvas')
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize( window.innerWidth, window.innerHeight );	// Make renderer fullscreen
+renderer.setAnimationLoop( update );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const textureLoader = new THREE.TextureLoader();
 
-const light = new THREE.AmbientLight( 0x404040 );
-scene.add( light );
+const controls = new OrbitControls(camera, renderer.domElement);
 
-camera.position.z = 5;
+const torusGeo = new THREE.TorusGeometry( 10, 3, 16, 100 );
+const material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
+const torus = new THREE.Mesh( torusGeo, material );
+scene.add( torus );
 
-function animate() {
+const bgSphereGeo = new THREE.SphereGeometry(100, 32, 32);
+const bgSphereTex = textureLoader.load('/test.jpg');
+const bgSphereMat = new THREE.MeshBasicMaterial({map: bgSphereTex, side: THREE.BackSide});
+const bgSphereMesh = new THREE.Mesh(bgSphereGeo, bgSphereMat);
+scene.add(bgSphereMesh);
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
 
+
+const ambientLight = new THREE.AmbientLight( 0xffffff );
+scene.add( ambientLight );
+
+const pointLight = new THREE.PointLight(0x00ff00);
+pointLight.position.set(20, 5, 5);
+scene.add(pointLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+scene.add(lightHelper);
+
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(gridHelper);
+
+
+function update() {
+
+	torus.rotation.x += 0.01;
+	torus.rotation.y += 0.01;
+
+	controls.update();
+
+	draw();
+}
+
+function draw() {
 	renderer.render( scene, camera );
-
 }
